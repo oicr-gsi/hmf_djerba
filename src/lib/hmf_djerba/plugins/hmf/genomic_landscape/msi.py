52c2da6cd2c6c19422655e29b4d3504bb2d4e661
Hanlin Chen <h2chen@oicr.on.ca>
2026-01-29 16:26:20 -0500

    Genomic_landscape plugin modifications to support HMF MSI reporting

--- src/lib/hmf_djerba/plugins/hmf/genomic_landscape/msi.py
+++ src/lib/hmf_djerba/plugins/hmf/genomic_landscape/msi.py
@@ -8,7 +8,7 @@ import os
 
 import numpy
 
-import djerba.plugins.genomic_landscape.constants as constants
+import hmf_djerba.plugins.hmf.genomic_landscape.constants as constants
 from djerba.util.image_to_base64 import converter
 from djerba.util.logger import logger
 from djerba.util.subprocess_runner import subprocess_runner
@@ -22,13 +22,13 @@ class msi_processor(logger):
         self.logger = self.get_logger(log_level, __name__, log_path)
         self.validator = path_validator(log_level, log_path)
 
-    def run(self, work_dir, r_script_dir, msi_file, biomarkers_path, tumour_id):
+    def run(self, work_dir, msi_file, biomarkers_path, tumour_id):
         """
           Runs all functions below.
           Assembles a chunk of json.
           """
         msi_summary = self.preprocess_msi(work_dir, msi_file)
-        msi_data = self.assemble_MSI(work_dir, r_script_dir, msi_summary)
+        msi_data = self.assemble_MSI(work_dir, msi_summary)
 
         # Write to genomic biomarkers maf if MSI is actionable
         if msi_data[constants.METRIC_ACTIONABLE]:
@@ -41,54 +41,60 @@ class msi_processor(logger):
 
     def preprocess_msi(self, work_dir, msi_file):
         """
-          summarize msisensor file
-          """
+          summarize purple MSI-related values
+        """
         out_path = os.path.join(work_dir, 'msi.txt')
-        msi_boots = []
         self.validator.validate_output_dir(work_dir)
         self.validator.validate_input_file(msi_file)
+        
         with open(msi_file, 'r') as msi_file:
-            reader_file = csv.reader(msi_file, delimiter="\t")
-            for row in reader_file:
-                msi_boots.append(float(row[3]))
-        msi_perc = numpy.percentile(numpy.array(msi_boots), [0, 25, 50, 75, 100])
+            reader_file = list(csv.reader(msi_file, delimiter="\t"))
+            purple_msi_header = reader_file[0]
+            purple_msi_data = reader_file[1]
+            
+            # Find the column positions
+            idx1 = purple_msi_header.index("msIndelsPerMb")
+            idx2 = purple_msi_header.index("msStatus")
+        
+            # Store the values in a list
+            msi_perc = [purple_msi_data[idx1], purple_msi_data[idx2]]
+
         with open(out_path, 'w') as out_file:
-            print("\t".join([str(item) for item in list(msi_perc)]), file=out_file)
+            print("\t".join([str(item) for item in msi_perc]), file=out_file)
         return out_path
 
-    def assemble_MSI(self, work_dir, r_script_dir, msi_summary):
-        msi_value = self.extract_MSI(work_dir, msi_summary)
-        msi_dict = self.call_MSI(msi_value)
-        msi_plot_location = self.write_biomarker_plot(work_dir, r_script_dir, "msi")
-        msi_dict[constants.METRIC_PLOT] = converter().convert_svg(msi_plot_location, 'MSI plot')
+    def assemble_MSI(self, work_dir, msi_summary):
+        msi_value, msi_status = self.extract_MSI(work_dir, msi_summary)
+        msi_dict = self.call_MSI(msi_value, msi_status)
+        
         return msi_dict
 
-    def call_MSI(self, msi_value):
-        """convert MSI percentage into a Low, Inconclusive or High call"""
+    def call_MSI(self, msi_value, msi_status):
+        """convert MSI percentage into a Low, Unknown or High call"""
         msi_dict = {constants.ALT: constants.MSI,
                     constants.ALT_URL: "https://www.oncokb.org/gene/Other%20Biomarkers/MSI-H",
                     constants.METRIC_VALUE: msi_value
                     }
         try:
-            if msi_value >= constants.MSI_CUTOFF:
+            if msi_status == "MSI":
                 msi_dict[constants.METRIC_ACTIONABLE] = True
                 msi_dict[constants.METRIC_ALTERATION] = "MSI-H"
                 msi_dict[constants.METRIC_TEXT] = "Microsatellite Instability High (MSI-H)"
-            elif constants.MSI_CUTOFF > msi_value >= constants.MSS_CUTOFF:
+            elif msi_status == "UNKNOWN":
                 msi_dict[constants.METRIC_ACTIONABLE] = False
-                msi_dict[constants.METRIC_ALTERATION] = "INCONCLUSIVE"
-                msi_dict[constants.METRIC_TEXT] = "Inconclusive Microsatellite Instability status"
-            elif msi_value < constants.MSS_CUTOFF:
+                msi_dict[constants.METRIC_ALTERATION] = "UNKNOWN"
+                msi_dict[constants.METRIC_TEXT] = "Unknown Microsatellite Instability status"
+            elif msi_status == "MSS":
                 msi_dict[constants.METRIC_ACTIONABLE] = False
                 msi_dict[constants.METRIC_ALTERATION] = "MSS"
                 msi_dict[constants.METRIC_TEXT] = "Microsatellite Stable (MSS)"
             else:
                 # shouldn't happen, but include for completeness
-                msg = f'Cannot evaluate for MSI value {msi_value}, MSI cutoff {constants.MSI_CUTOFF}, MSS cutoff {constants.MSS_CUTOFF}'
+                msg = f'Cannot evaluate for MSI status {msi_status}'
                 self.logger.error(msg)
                 raise RuntimeError(msg)
         except TypeError:
-            msg = "Illegal value '{0}' extracted from file for MSI; must be a number".format(msi_value)
+            msg = "Illegal value '{0}' extracted from file for MSI; must be a string".format(msi_status)
             self.logger.error(msg)
             raise RuntimeError(msg)
         return (msi_dict)
@@ -101,20 +107,12 @@ class msi_processor(logger):
             reader_file = csv.reader(msi_file, delimiter="\t")
             for row in reader_file:
                 try:
-                    msi_value = float(row[2])
+                    msi_value = float(row[0])
+                    msi_status = str(row[1])
                 except IndexError as err:
-                    msg = "Incorrect number of columns in msisensor row: '{0}'".format(row) + \
+                    msg = "Incorrect number of columns in msi file: '{0}'".format(row) + \
                           "read from '{0}'".format(os.path.join(work_dir, constants.MSI_FILE_NAME))
                     self.logger.error(msg)
                     raise RuntimeError(msg) from err
-        return msi_value
+        return msi_value, msi_status
 
-    def write_biomarker_plot(self, work_dir, r_script_dir, marker):
-        out_path = os.path.join(work_dir, marker + '.svg')
-        args = [
-            os.path.join(r_script_dir, 'msi_plot.R'),
-            '-d', work_dir
-        ]
-        subprocess_runner(self.log_level, self.log_path).run(args)
-        self.logger.info("Wrote msi plot to {0}".format(out_path))
-        return out_path
--- src/lib/hmf_djerba/plugins/hmf/genomic_landscape/render.py
+++ src/lib/hmf_djerba/plugins/hmf/genomic_landscape/render.py
@@ -39,12 +39,20 @@ class html_builder:
                     self.logger.error(msg)
                     raise ValueError(msg)
                     
-            elif marker == "MSI" and not can_report_msi:
-                cells = [
-                    hb.td(info[constants.ALT]),
-                    hb.td("NA"),
-                    hb.td("Cancer cell content below threshold to call MS score; must be &#8805;50&#37;")
-                ]
+            elif marker == "MSI":
+                if not can_report_msi:
+                    cells = [
+                        hb.td(info[constants.ALT]),
+                        hb.td("NA"),
+                        hb.td("Cancer cell content below threshold to call MS score; must be &#8805;50&#37;")
+                    ]
+                else:
+                    cells = [
+                        hb.td(info[constants.ALT]),
+                        hb.td(info[constants.METRIC_ALTERATION]),
+                        hb.td(info[constants.METRIC_TEXT])
+                    ]
+
             else:
                 cells = [
                     hb.td(info[constants.ALT]),
